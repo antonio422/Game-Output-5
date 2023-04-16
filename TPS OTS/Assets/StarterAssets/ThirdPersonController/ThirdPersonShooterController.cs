798e8ab02531c4350c0a84f619e50df55698acd4
masoyes1 <masomilliano.crisostomo@obf.ateneo.edu>
2023-04-16 23:04:40 +0800

    Added scene, player, and shooting ability

---
+++ TPS OTS/Assets/StarterAssets/ThirdPersonController/ThirdPersonShooterController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
+    [SerializeField] private Transform pfBulletProjectile;
+    [SerializeField] private Transform spawnBulletPosition;
+
+    private StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)) {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
+        Vector3 worldAimTarget = mouseWorldPosition;
+        worldAimTarget.y = transform.position.y;
+        Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+        transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+    
+        if (starterAssetsInputs.shoot) {
+            Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
+            Instantiate(pfBulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            starterAssetsInputs.shoot = false;
+        }
+    }
+}
